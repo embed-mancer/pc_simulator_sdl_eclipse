a488b4567fcff92cc18a6bd199fef04990a81071
Nemausa <tappanmorris@outlook.com>
2024-11-12 09:32:52 +0800

    optimize settings
    Signed-off-by: Nemausa <tappanmorris@outlook.com>

--- lvgl/demos/cell/menu/settings.c
+++ lvgl/demos/cell/menu/settings.c
@@ -4,10 +4,14 @@
 #include "preview.h"
 #include "left_menu.h"
 
-static bool is_active = false;
+static const int LABEL_OFFSET_Y = 60;
+static const int LABEL_BASE_Y = 20;
+static const int LABEL_WIDTH = 250;
+
 extern lv_obj_t* menu_window;
 
 static lv_obj_t* elements[14] = {NULL};
+static bool is_active = false;
 
 static void open_window();
 
@@ -22,52 +26,56 @@ static bool handle_click_event(const click_t click) {
     case CLICK_SHORT_BACK:
       is_active = false;
       break;
-    case CLICK_LONG_SET:
+    case CLICK_SHORT_UP:
       break;
-    case CLICK_LONG_BACK:
+    case CLICK_SHORT_DOWN:
       break;
     default:
       return false;
   }
-  if (!is_active) return false;
-  return true;
+  return is_active;
 }
 
 static void toggle_day_night() {}
 
 static void destroy() {}
 
-static void open_window() {
+static void prepare_window() {
   left_menu_show(false);
   preview_close();
-  label_params_t params_connection =
-      ui_helpers_settings_params(250, 20, lang_text(TEXT_ID_DEVICE_CONNECTION),
-                        RIGHT_ARROW_IMG_PATH, NULL);
-  ui_helpers_component(menu_window, &elements[0], &elements[1],
-                       params_connection);
-  label_params_t params_option_1 = ui_helpers_settings_params(
-      250, 80, lang_text(TEXT_ID_OPTIONAL_INFO_1), RIGHT_ARROW_IMG_PATH, NULL);
-  ui_helpers_component(menu_window, &elements[2], &elements[3],
-                       params_option_1);
-  label_params_t params_option_2 = ui_helpers_settings_params(
-      250, 140, lang_text(TEXT_ID_OPTIONAL_INFO_2), RIGHT_ARROW_IMG_PATH, NULL);
-  ui_helpers_component(menu_window, &elements[4], &elements[5],
-                       params_option_2);
-  label_params_t params_auto =
-      ui_helpers_settings_params(250, 200, lang_text(TEXT_ID_AUTO_BRIGHTNESS), NULL,
-                        lang_text(TEXT_ID_OPEN));
-  ui_helpers_component(menu_window, &elements[6], &elements[7], params_auto);
-  label_params_t params_unit = ui_helpers_settings_params(
-      250, 260, lang_text(TEXT_ID_UNIT_SETTINGS), RIGHT_ARROW_IMG_PATH, NULL);
-  ui_helpers_component(menu_window, &elements[8], &elements[9], params_unit);
-  label_params_t params_time = ui_helpers_settings_params(
-      250, 320, lang_text(TEXT_ID_TIME_SETTINGS), RIGHT_ARROW_IMG_PATH, NULL);
-  ui_helpers_component(menu_window, &elements[10], &elements[11], params_time);
-  label_params_t params_language =
-      ui_helpers_settings_params(250, 380, lang_text(TEXT_ID_LANGUAGE_SETTINGS),
-                        RIGHT_ARROW_IMG_PATH, NULL);
-  ui_helpers_component(menu_window, &elements[12], &elements[13],
-                       params_language);
+}
+
+static void setup_label(lv_obj_t* window, lv_obj_t** element1,
+                        lv_obj_t** element2, int y_offset, int text_id,
+                        const char* img_path, const char* text) {
+  label_params_t params = ui_helpers_settings_params(
+      LABEL_WIDTH, y_offset, lang_text(text_id), img_path, text);
+  ui_helpers_component(window, element1, element2, params);
+}
+
+static void open_window() {
+  prepare_window();
+
+  setup_label(menu_window, &elements[0], &elements[1], LABEL_BASE_Y,
+              TEXT_ID_DEVICE_CONNECTION, RIGHT_ARROW_IMG_PATH, NULL);
+  setup_label(menu_window, &elements[2], &elements[3],
+              LABEL_BASE_Y + LABEL_OFFSET_Y, TEXT_ID_OPTIONAL_INFO_1,
+              RIGHT_ARROW_IMG_PATH, NULL);
+  setup_label(menu_window, &elements[4], &elements[5],
+              LABEL_BASE_Y + 2 * LABEL_OFFSET_Y, TEXT_ID_OPTIONAL_INFO_2,
+              RIGHT_ARROW_IMG_PATH, NULL);
+  setup_label(menu_window, &elements[6], &elements[7],
+              LABEL_BASE_Y + 3 * LABEL_OFFSET_Y, TEXT_ID_AUTO_BRIGHTNESS, NULL,
+              lang_text(TEXT_ID_OPEN));
+  setup_label(menu_window, &elements[8], &elements[9],
+              LABEL_BASE_Y + 4 * LABEL_OFFSET_Y, TEXT_ID_UNIT_SETTINGS,
+              RIGHT_ARROW_IMG_PATH, NULL);
+  setup_label(menu_window, &elements[10], &elements[11],
+              LABEL_BASE_Y + 5 * LABEL_OFFSET_Y, TEXT_ID_TIME_SETTINGS,
+              RIGHT_ARROW_IMG_PATH, NULL);
+  setup_label(menu_window, &elements[12], &elements[13],
+              LABEL_BASE_Y + 6 * LABEL_OFFSET_Y, TEXT_ID_LANGUAGE_SETTINGS,
+              RIGHT_ARROW_IMG_PATH, NULL);
 }
 
 static void close_window() {}
